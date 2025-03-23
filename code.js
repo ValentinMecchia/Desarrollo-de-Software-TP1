90f5215564dbefb63a5dbf059115db2ecddef8b0
ValentinMecchia <valentinmecchia@gmail.com>
2025-03-23 04:06:15 -0300

    transitions and responsivness added

---
+++ code.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function() {
+    async function loadSections() {
+        const headerResponse = await fetch("./html/header.html");
+        const headerHtml = await headerResponse.text();
+        document.querySelector("header").innerHTML = headerHtml;
+
+        const footerResponse = await fetch("./html/footer.html");
+        const footerHtml = await footerResponse.text();
+        document.querySelector("footer").innerHTML = footerHtml;
+
+        setupNavbarScroll();
+        setupHamburgerMenu();
+    }
+
+    function setupNavbarScroll() {
+        let lastScrollTop = 0;
+        const navbar = document.querySelector('.navbar');
+        const threshold = 50;
+
+        window.addEventListener('scroll', function() {
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            if (scrollTop - lastScrollTop > threshold) {
+                navbar.style.top = '-100px';
+                lastScrollTop = scrollTop;
+            } else if (lastScrollTop - scrollTop > threshold) {
+                navbar.style.top = '0';
+                lastScrollTop = scrollTop;
+            }
+        });
+    }
+    loadSections();
+});
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const form = event.target;
+    const data = new FormData(form);
+    const formData = new URLSearchParams(data).toString();
+
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then(response => {
+        if (response.ok) {
+            form.reset();
+            alert("Mensaje enviado correctamente");
+        } else {
+            alert("Error al enviar el mensaje");
+        }
+    });
+});
+
+document.getElementById("send-button").addEventListener("mousedown", function() {
+    document.getElementById("send-button").style.boxShadow = "none"
+});
+
+document.getElementById("send-button").addEventListener("mouseup", function() {
+    document.getElementById("send-button").style.boxShadow = "5px 5px 0 var(--third-text-color)";
+});
